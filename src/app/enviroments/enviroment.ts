99fa964260b7b7d9cbe63978cf6650672523da46
Al-hussein Abdullah <al-husseinabdullah@Al-husseins-Mac-Studio.local>
2024-11-06 05:04:14 +0300

    eeee

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -2,11 +2,4 @@ export const environment = {
     production: false,
     apiUrl: 'http://192.168.1.78:3000/api',
 
-}; export const environmentoffice = {
-    production: false,
-    apiUrl: 'http://192.168.0.198:3000/api',
-};
-export const environmentserver = {
-    production: false,
-    apiUrl: 'http://192.168.1.78:3000/api'  // Replace with your PC’s IP
-};
+}; 
