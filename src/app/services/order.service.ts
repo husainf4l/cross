14e61f6ffb35358864b7aa4e72fcb48acda88b20
Al-hussein Abdullah <al-husseinabdullah@Al-husseins-Mac-Studio.local>
2024-11-06 05:16:37 +0300

    eeee

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Order } from './models/interfaces.model';
-import { environment } from '../environments/enviroment';
+import { environment } from '../environments/environment.prod';
 
 
 @Injectable({
